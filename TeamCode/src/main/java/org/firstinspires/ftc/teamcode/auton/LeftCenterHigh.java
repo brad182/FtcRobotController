00395c70239f7469665714db53a273cac8e19c1d
blueseaturtle08 <113743479+blueseaturtle08@users.noreply.github.com>
2022-11-15 19:52:02 -0800

    Update LeftCenterHigh.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/LeftCenterHigh.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/LeftCenterHigh.java
@@ -7,14 +7,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
-@Autonomous(name = "Left Center High Junction", group = "Autonomous")
-public class LeftCenterHigh extends LinearOpMode {
+import java.util.ArrayList;
 
+@Autonomous(name = "LeftCenterHigh", group = "Vision")
+public class LeftCenterHigh extends LinearOpMode
+{
   private BNO055IMU imu;
   private DcMotor frontLeftMotor;
   private DcMotor backLeftMotor;
@@ -23,19 +30,46 @@ public class LeftCenterHigh extends LinearOpMode {
   public int CurrentTargetAngle = 0;
   public DcMotor liftMotor = null;
   public Servo clawMotor = null;
-  
+
   static int[] clawToggle = {0, 1};
 
   public static final int HI = 3000; //hi value
   public static final int GR = 00; //ground value
-  
+
   int clawSwitch = 1;
   int liftPosition = 0;
   String v = "";
 
+  OpenCvCamera camera;
+  org.firstinspires.ftc.teamcode.auton.AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+  static final double FEET_PER_METER = 3.28084;
+
+  // Lens intrinsics
+  // UNITS ARE PIXELS
+  // NOTE: this calibration is for the C920 webcam at 800x448.
+  // You will need to do your own calibration for other configurations!
+  double fx = 578.272;
+  double fy = 578.272;
+  double cx = 402.145;
+  double cy = 221.506;
+
+  // UNITS ARE METERS
+  double tagsize = 0.166;
+
+  // Tag ID 1, 2, 3 from the 36h11 family
+  AprilTagDetection tagOfInterest = null;
+  int LEFT = 1;
+  int MIDDLE = 2;
+  int RIGHT = 3;
+
+
+
+
   @Override
-  public void runOpMode() {
-    
+  public void runOpMode()
+  {
+
     BNO055IMU.Parameters imuParameters;
 
     imu = hardwareMap.get(BNO055IMU.class, "imu");
@@ -62,14 +96,114 @@ public class LeftCenterHigh extends LinearOpMode {
     // Prompt user to press start buton.
     //telemetry.addData("IMU Example", "Press start to continue...");
     telemetry.update();
-    
+
     clawMotor.setPosition(1);
 
-    waitForStart();
+    int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+    camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+    aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+    //org.firstinspires.ftc.teamcode.auton.
 
+    camera.setPipeline(aprilTagDetectionPipeline);
+    camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+    {
+      @Override
+      public void onOpened()
+      {
+        camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+      }
 
-    if (opModeIsActive()) {
-      // Put run blocks here.
+      @Override
+      public void onError(int errorCode)
+      {
+
+      }
+    });
+
+    telemetry.setMsTransmissionInterval(50);
+
+    /*
+     * The INIT-loop:
+     * This REPLACES waitForStart!
+     */
+    while (!isStarted() && !isStopRequested())
+    {
+      ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+      if(currentDetections.size() != 0)
+      {
+        boolean tagFound = false;
+
+        for(AprilTagDetection tag : currentDetections)
+        {
+          if(tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT)
+          {
+            tagOfInterest = tag;
+            tagFound = true;
+            break;
+          }
+        }
+
+        if(tagFound)
+        {
+          telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+          tagToTelemetry(tagOfInterest);
+        }
+        else
+        {
+          telemetry.addLine("Don't see tag of interest :(");
+
+          if(tagOfInterest == null)
+          {
+            telemetry.addLine("(The tag has never been seen)");
+          }
+          else
+          {
+            telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+            tagToTelemetry(tagOfInterest);
+          }
+        }
+
+      }
+      else
+      {
+        telemetry.addLine("Don't see tag of interest :(");
+
+        if(tagOfInterest == null)
+        {
+          telemetry.addLine("(The tag has never been seen)");
+        }
+        else
+        {
+          telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+          tagToTelemetry(tagOfInterest);
+        }
+
+      }
+
+      telemetry.update();
+      sleep(20);
+    }
+
+    /*
+     * The START command just came in: now work off the latest snapshot acquired
+     * during the init loop.
+     */
+
+    /* Update the telemetry */
+    if(tagOfInterest != null)
+    {
+      telemetry.addLine("Tag snapshot:\n");
+      tagToTelemetry(tagOfInterest);
+      telemetry.update();
+    }
+    else
+    {
+      telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+      telemetry.update();
+    }
+
+    while(opModeIsActive()) {
       frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
       backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
       backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -93,24 +227,45 @@ public class LeftCenterHigh extends LinearOpMode {
       clawMotor.setPosition(1);
       sleep(300);
       Backward(2);
-      
-      if(v.equals("red")){
-        Right(12);
-      }
-      else if(v.equals("green")){
-        Right(36);
-      }
-      else if(v.equals("blue")){
-        Right(60);
-      }
 
-      
+      if (tagOfInterest == null || tagOfInterest.id == LEFT) {
+        // pathing for one dot
+        telemetry.addLine("One Dot");
+        telemetry.update();
+
+        sleep(1000);
+
+      } else if (tagOfInterest.id == MIDDLE) {
+        // pathing for two dots
+        telemetry.addLine("Two Dots");
+        telemetry.update();
+
+        sleep(1000);
+      } else {
+        // pathing for three dots
+        telemetry.addLine("Three Dots");
+        telemetry.update();
+
+        sleep(1000);
       }
+    }
+
+        /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending
+        while (opModeIsActive()) {sleep(20);}
+         */
   }
 
-  /**
-  * Describe this function...
-  */
+  void tagToTelemetry(AprilTagDetection detection)
+  {
+    telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+    telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+    telemetry.addLine(String.format("Translation Y: %.2f feet", detection                               .pose.y*FEET_PER_METER));
+    telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+    telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+    telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+    telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+  }
+
   private void ResetEncoders(){
     backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -131,7 +286,7 @@ public class LeftCenterHigh extends LinearOpMode {
     imuParameters.loggingEnabled = false;
     // Initialize IMU.
     imu.initialize(imuParameters);
-    
+
   }
   private void Forward(int Inches) {
     Orientation angles;
@@ -212,6 +367,7 @@ public class LeftCenterHigh extends LinearOpMode {
     frontLeftMotor.setPower(0);
     frontRightMotor.setPower(0);
   }
+
   private void TurnRight(int TargetAngle) {
     CurrentTargetAngle += TargetAngle;
     Orientation angles;
@@ -263,5 +419,5 @@ public class LeftCenterHigh extends LinearOpMode {
   private void clawClose(){
     clawMotor.setPosition(0);
   }
-
 }
+
