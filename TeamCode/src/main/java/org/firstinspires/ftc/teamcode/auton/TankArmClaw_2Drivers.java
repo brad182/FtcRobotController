435e4bc06066a48699b6ff0cdb9d79a45b11709e
mepotato <nicole216lee@gmail.com>
2022-11-27 14:24:01 -0800

    added second claw motor to driver

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/TankArmClaw_2Drivers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/TankArmClaw_2Drivers.java
@@ -15,7 +15,8 @@ public class TankArmClaw_2Drivers extends LinearOpMode {
     public DcMotor backRightMotor = null;
     public DcMotor frontRightMotor = null;
     public DcMotor liftMotor = null;
-    public Servo clawMotor = null;
+    public Servo leftClawMotor = null;
+    public Servo rightClawMotor = null;
 
     static final double[] speed = {1.0, 0.15};
     static final double[] toggleDirection = {1.0, -1.0};
@@ -39,7 +40,8 @@ public class TankArmClaw_2Drivers extends LinearOpMode {
         backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
         liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
-        clawMotor = hardwareMap.get(Servo.class, "clawMotor");
+        leftClawMotor = hardwareMap.get(Servo.class, "clawMotor");
+        rightClawMotor = hardwareMap.get(Servo.class, "clawMotor");
         liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -151,9 +153,10 @@ public class TankArmClaw_2Drivers extends LinearOpMode {
             
             telemetry.addData("clawSwitch", clawSwitch);
             telemetry.update();
-            
-            clawMotor.setPosition(clawSwitch);
-            
+
+            leftClawMotor.setPosition(clawSwitch);
+            rightClawMotor.setPosition(clawSwitch);
+
             
             
             
