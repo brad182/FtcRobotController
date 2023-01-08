1f45f5c4200f805520b25673d98e8488de47d85e
Brad <81612365+brad182@users.noreply.github.com>
2023-01-08 15:58:30 -0800

    Roadrunner Setup Done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
@@ -41,7 +41,7 @@ public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
     public static double PARALLEL_X = 1.175; // X is the up and down direction
-    public static double PARALLEL_Y = -5.375; // Y is the strafe direction
+    public static double PARALLEL_Y = 5.375; // Y is the strafe direction
 
     public static double PERPENDICULAR_X = -7.7;
     public static double PERPENDICULAR_Y = 0.5;
